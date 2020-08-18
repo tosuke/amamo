89f58ed34be2827a663abcf01f2dc9205a9105be
Tosuke <tasukeprg@gmail.com>
2020-08-19 05:23:34 +0900

    chore(build): use css-minimizer-webpack-plugin instead of optimize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
 const CSSExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const CSSMinimizePlugin = require('css-minimizer-webpack-plugin');
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const DotenvPlugin = require('dotenv-webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -57,7 +57,7 @@ module.exports = {
     }),
   ],
   optimization: {
-    minimizer: [new TerserPlugin(), new OptimizeCSSPlugin()],
+    minimizer: [new TerserPlugin(), new CSSMinimizePlugin()],
     // Granular Chunks
     // from https://glitch.com/edit/#!/webpack-granular-split-chunks?path=webpack.config.js
     splitChunks: {
